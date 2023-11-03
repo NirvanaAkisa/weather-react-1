e230357f9207aa12a0cd21a0621de096e12e6efd
NirvanaAkisa <137563355+NirvanaAkisa@users.noreply.github.com>
2023-11-03 21:51:50 +0200

    udded my github link

--- weather-react-1/src/index.js
+++ weather-react-1/src/index.js
@@ -1,15 +1,11 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDom from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-
-root.render(
-  <StrictMode>
-      <BrowserRouter>
-      </BrowserRouter>
-      , document.querySelector("#weather")
-
-  </StrictMode>
+ReactDom.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.querySelector("#weather")
 );
